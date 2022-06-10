6d5bdb55d1f7953abed9c36db74437017f07e483
Olga Balsalobre-Ruza <47603865+olgabalsa@users.noreply.github.com>
2022-06-10 13:00:53 +0200

    Update run_MCMC.py

--- modules_KOBEsim/run_MCMC.py
+++ modules_KOBEsim/run_MCMC.py
@@ -22,7 +22,7 @@ from matplotlib.patches import ConnectionPatch
 # Model and functions to run the MCMC
 #=====================================
 
-#=====================================
+
 """
 Parameters of the RV model:
 ---------------------------
